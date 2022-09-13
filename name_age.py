8c75e4123f7ea39a45ed06645c4ec5e818a59e8e
benrmartins <benrmartins@gmail.com>
2022-09-13 11:10:14 -0400

    Initial commit

---
+++ name_age.py
@@ -0,0 +1,9 @@
+import stdio
+import sys
+
+# Accept name (str) and age (str) as command-line arguments.
+name = ...
+age = ...
+
+# Write the message "name is age years old." to standard output.
+stdio.writeln(...)
